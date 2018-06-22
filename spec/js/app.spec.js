2e1027476a8f2e3c0cb85c053a531df09ca9da32
nzequestrian <engravethis@yahoo.com>
2018-06-22 10:17:53 -0400

    INITIAL COMMIT

---
+++ spec/js/app.spec.js
@@ -0,0 +1,5 @@
+// Delete a card when the 'x' button is click
+// Display navigation when menu button is clicked
+// Make a new card with the 'New Card' form
+// Add 'active' class to navigation elements when they are clicked
+// Remove 'active' class from other navigation elements when one is clicked
