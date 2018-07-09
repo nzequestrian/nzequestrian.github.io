0fffa6242a6bcdd958056cc146800277f1faa553
nzequestrian <engravethis@yahoo.com>
2018-07-09 16:41:51 -0400

    final

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
-$('body').scrollspy({ target: '#main-nav' })
 
-$(document).ready(function () {
-    $("li").click(function () {
-        $('li > ul').hide();
-        $(this).children("ul").toggle();
-    });
-});
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", dropDownText);
+}
+
+function dropDownText() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+// -------------------------------------------------------------------
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// ------------------------------------------
+
